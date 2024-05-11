9f1b31d193e8d91643e1806b469f71b7b4f0fd48
Thomas2424 <113831322+Tomas-618@users.noreply.github.com>
2024-05-12 03:52:21 +0400

    patch 20

--- Assets/Scripts/Entity/Explosion.cs
+++ Assets/Scripts/Entity/Explosion.cs
@@ -6,10 +6,18 @@ public class Explosion : MonoBehaviour
 {
     [SerializeField, Min(0)] private float _lifeTime;
 
+    private Coroutine _coroutine;
+
     public event Action<Explosion> LifeTimeEnded;
 
-    private void Start() =>
-        StartCoroutine(ProcessLifeTime(_lifeTime));
+    private void OnEnable() =>
+        _coroutine = StartCoroutine(ProcessLifeTime(_lifeTime));
+
+    private void OnDisable()
+    {
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+    }
 
     private IEnumerator ProcessLifeTime(float value)
     {
