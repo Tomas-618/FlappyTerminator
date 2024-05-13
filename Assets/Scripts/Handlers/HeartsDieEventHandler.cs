b2448efe040e2cc4e2918fa89f0734c48e8a5a58
Thomas2424 <113831322+Tomas-618@users.noreply.github.com>
2024-05-13 17:57:24 +0400

    patch 35.5

--- Assets/Scripts/Handlers/HeartsDieEventHandler.cs
+++ Assets/Scripts/Handlers/HeartsDieEventHandler.cs
@@ -4,7 +4,6 @@ using AYellowpaper;
 public abstract class HeartsDieEventHandler : MonoBehaviour
 {
     [SerializeField] private InterfaceReference<IReadOnlyHeartsEvents, MonoBehaviour> _events;
-    //[SerializeField] private Fabric<Explosion> _explosionEffectsFabric;
 
     protected virtual void OnEnable() =>
         _events.Value.Died += DoActionOnDeath;
@@ -18,12 +17,4 @@ public abstract class HeartsDieEventHandler : MonoBehaviour
     }
      
     protected abstract void DoActionOnDeath();
-
-    //private void Die()
-    //{
-    //    Explosion explosion = _explosionEffectsFabric.Create();
-
-    //    explosion.transform.position = _transform.position;
-    //    explosion.gameObject.SetActive(true);
-    //}
 }
